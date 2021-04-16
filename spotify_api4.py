c685a6d7e9e88635f60aa4b11b29f675e7660752
WHWH10 <eyet010@gmail.com>
2021-04-16 13:13:52 +0900

    Update spotify_api4.py

--- spotify_api4.py
+++ spotify_api4.py
@@ -5,8 +5,8 @@ import json
 import logging
 import pymysql
 
-client_id = "cb60d784b77842e6a585a4b770c42769"
-client_secret = "a593703d01dd4b82ace1e70dad3d99c3"
+client_id = ""
+client_secret = ""
 
 host = "localhost"
 port = 3306
